8be6df8d0b6ef3913a20e215b1d5f69673d02c35
CSKYScarlet <108151543+CSKYScarlet@users.noreply.github.com>
2022-07-05 20:32:02 +0900

    Add files via upload

--- random.py
+++ random.py
@@ -10,11 +10,16 @@ def random_things():
     return random_value
     
 sub_num = 0
+list_num = [0]
 for value in range(6):
     
     ran_num = random_things()
-    if sub_num == ran_num:
-        ran_num = random_things()
+    for list_value in list_num:
+        if list_value == ran_num:
+            ran_num = random_things()
+        
+    # if sub_num == ran_num:
+    #     ran_num = random_things()
         
     print(ran_num)
-    sub_num = ran_num
+    list_num.append(ran_num)
