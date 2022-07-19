3490ce3f23c0bfa6e90f48e5e717dd99914ee9fc
CSKYScarlet <108151543+CSKYScarlet@users.noreply.github.com>
2022-07-19 10:02:48 +0900

    Add files via upload

--- random.py
+++ random.py
@@ -1,6 +1,7 @@
 import random
 
-num_value = 45
+random_range = int(input("랜덤을 돌릴 횟수"))
+num_value = int(input("랜덤의(최대) 범위"))
 
 # ran_num = random.randrange(1, (num_value + 1))
 # print(ran_num)
@@ -11,15 +12,19 @@ def random_things():
     
 sub_num = 0
 list_num = [0]
-for value in range(6):
+rerandom = 0
+for value in range(random_range):
     
     ran_num = random_things()
     for list_value in list_num:
         if list_value == ran_num:
-            ran_num = random_things()
-        
+            rerandom += 1
+            print(rerandom, "번 다시 돌림", "(" + str(ran_num) + ")")
+            break
     # if sub_num == ran_num:
     #     ran_num = random_things()
         
+    if list_value == ran_num:
+        continue
     print(ran_num)
     list_num.append(ran_num)
